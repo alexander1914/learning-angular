118579b956469fa3cf717a6e1681aea687973967
Alexander Oliveira <acc01719@sky.com.br>
2022-09-26 15:18:03 -0300

    I am learning about component

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template:`
+  <div><h1>{{pageTitle}}</h1>
+      <div>My First Component</div>
+  <div>
+  `
 })
-export class AppComponent {
-  title = 'my-store';
+
+export class AppComponent{
+  pageTitle: string = "Nerd Shoes Geek";
 }
